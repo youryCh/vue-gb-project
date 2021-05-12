308863b65cb5790fc27b614aa3be816ae83b66e0
youryCh <xolmes@ya.ry>
2021-05-13 00:00:03 +0300

    add keybord

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
     <div>
-      <input type="text" v-model.number="operand1">
+      <input class="input" type="text" v-model="operand1">
       {{ operator }}
-      <input type="text" v-model.number="operand2">
+      <input class="input" type="text" v-model="operand2">
       = {{ result || 0 }}
     </div>
     <div class="btns">
-      <button @click="calculate($event)" v-for="(button, index) in buttons" :key="index">{{ button }}</button>
+      <button v-for="button in buttons" @click="calculate" :key="button">{{ button }}</button>
+    </div>
+    <div class="errorMsg" v-if="error">
+      {{ error }}
+    </div>
+    <label>
+      <input class="input_mg" type="checkbox" v-model="showKeybord">
+      Отобразить экранную клавиатуру
+    </label>
+    <div class="btns" v-if="showKeybord">
+      <button v-for="key in keys" @click="addNumber" :key="key">{{ key }}</button>
+      <button @click="clearInput">&#8592;</button>
+      <br>
+      <input type="radio" name="operand" id="op1" value="operand1" v-model="checkedOperand">
+      <label for="op1">Операнд 1</label>
+      <input type="radio" name="operand" id="op2" value="operand2" v-model="checkedOperand">
+      <label for="op2">Операнд 2</label>
     </div>
   </div>
 </template>
@@ -16,40 +32,65 @@
 export default {
   name: 'Calculator',
   data: () => ({
-    buttons: [
-      '+',
-      '-',
-      '/',
-      '*',
-      '^',
-      'div'
-    ],
+    buttons: ['+', '-', '/', '*', '^', 'div'],
     operand1: '',
     operand2: '',
     result: '',
-    operator: null
+    operator: '',
+    showKeybord: false,
+    keys: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    checkedOperand: 'operand1',
+    error: ''
   }),
   methods: {
     calculate (e) {
       this.operator = e.target.innerHTML
+      this.operand1 = Number(this.operand1)
+      this.operand2 = Number(this.operand2)
+      console.log('ok1')
       switch (this.operator) {
         case '+':
+          console.log('ok')
           this.result = this.operand1 + this.operand2
+          this.error = ''
           break
         case '-':
           this.result = this.operand1 - this.operand2
+          this.error = ''
           break
         case '/':
-          this.result = (this.operand1 / this.operand2).toFixed(2)
+          if (this.operand2 !== 0) {
+            this.result = Number((this.operand1 / this.operand2).toFixed(2))
+          } else {
+            this.error = 'На 0 делить нельзя'
+          }
           break
         case '*':
           this.result = this.operand1 * this.operand2
+          this.error = ''
           break
         case '^':
           this.result = Math.pow(this.operand1, this.operand2)
+          this.error = ''
           break
         case 'div':
-          this.result = (this.operand1 / this.operand2).toFixed(0)
+          this.result = Number((this.operand1 / this.operand2).toFixed(0))
+          this.error = ''
+      }
+    },
+    addNumber (event) {
+      const num = event.target.innerHTML
+      if (this.checkedOperand === 'operand1') {
+        this.operand1 += num
+      } else {
+        this.operand2 += num
+      }
+    },
+    clearInput () {
+      if (this.checkedOperand === 'operand1') {
+        this.operand1 = this.operand1.slice(0, -1)
+      } else {
+        this.operand2 = this.operand2.slice(0, -1)
       }
     }
   }
@@ -59,9 +100,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
 
-input
-  width: 60px
-  text-align: center
+.input
+  text-align: right
+
+  &_mg
+    margin-top: 50px
 
 .btns
   margin-top: 8px
@@ -69,4 +112,8 @@ input
 button
   margin-left: 5px
 
+.errorMsg
+  margin-top: 16px
+  border: 1px dotted red
+  color: red
 </style>
