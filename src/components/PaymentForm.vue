e58c18ec15b1afc58ce86c4e6f26f4329f4909db
youryCh <xolmes@ya.ry>
2021-05-15 23:16:16 +0300

    add buttom component

---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="[$style.paymentForm]">
+    <input :class="[$style.paymentForm__input]" type="text" placeholder="Payment date" v-model="date">
+    <input :class="[$style.paymentForm__input]" type="text" placeholder="Payment dascription" v-model="category">
+    <input :class="[$style.paymentForm__input]" type="text" placeholder="Payment amount" v-model.number="price">
+    <Button :text="formButtonText" @handler="save" />
+  </div>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  components: {
+    Button
+  },
+  data () {
+    return {
+      date: '',
+      category: '',
+      price: null,
+      formButtonText: 'ADD'
+    }
+  },
+  methods: {
+    save () {
+      const { date, category, price } = this
+      this.$emit('add', { date, category, price })
+      this.$parent.showForm = false
+      this.clearForm()
+    },
+    clearForm () {
+      this.date = ''
+      this.category = ''
+      this.price = null
+    }
+  }
+}
+</script>
+
+<style module lang="sass">
+  .paymentForm
+    display: flex
+    flex-direction: column
+    width: 50vw
+    position: fixed
+    left: 25vw
+    background-color: white
+    border: 1px solid #e0e0e0
+    border-radius: 5px
+    padding: 8px
+    box-shadow: 5px 5px 20px #e0e0e0
+
+    &__input
+      height: 40px
+      border: 1px solid #e0e0e0
+      border-radius: 3px
+      margin-bottom: 10px
+      outline: none
+      padding: 8px
+
+</style>
---
+++ src/components/PaymentsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="[$style.paymentsList]">
+    <div :class="[$style.payment]">
+      <h4>#</h4>
+      <h4>Date</h4>
+      <h4>Category</h4>
+      <h4>Value</h4>
+    </div>
+    <div :class="[$style.payment]" v-for="(item, index) in items" :key="index">
+      <div>{{ index + 1 }}</div>
+      <div>{{ item.date }}</div>
+      <div>{{ item.category }}</div>
+      <div>{{ item.price }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style module lang="sass">
+  .payment
+    display: grid
+    grid-template-columns: .4fr 2fr 2fr .5fr
+    border-bottom: 1px solid #e0e0e0
+    height: 40px
+    align-items: center
+    color: #345678
+
+</style>
